b2f3c66ac92b352ce64b34c64c22a1058a5fcb6d
Rachel Dowdall <rjdowdall@gmail.com>
2017-03-31 12:26:41 +0100

    Add d3 and put into class

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -17,3 +17,86 @@ function drawBuffer( width, height, context, data ) {
     }
 }
 
+
+
+var parseTime = d3.timeParse("%d-%b-%y");
+
+class D3Plot {
+    constructor(){
+        console.log("hello")
+        var svg = d3.select("svg"),
+            margin = {top: 20, right: 20, bottom: 30, left: 50}
+
+        this.width = +svg.attr("width") - margin.left - margin.right
+        this.height = +svg.attr("height") - margin.top - margin.bottom
+        
+        this.g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        
+
+        this.x = d3.scaleTime()
+            .rangeRound([0, this.width]);
+
+        this.y = d3.scaleLinear()
+            .rangeRound([this.height, 0]);
+
+    }
+	
+
+
+
+
+
+    draw(){
+
+        var self = this;
+        var line = d3.line()
+            .x(function(d) { return self.x(d.date); })
+            .y(function(d) { return self.y(d.close); });
+
+        d3.tsv("data.tsv", function(d) {
+          d.date = parseTime(d.date);
+          d.close = +d.close;
+          return d;
+        }, function(error, data) {
+          if (error) throw error;
+
+          self.x.domain(d3.extent(data, function(d) { return d.date; }));
+          self.y.domain(d3.extent(data, function(d) { return d.close; }));
+
+          self.g.append("g")
+              .attr("transform", "translate(0," + self.height + ")")
+              .call(d3.axisBottom(self.x))
+            .select(".domain")
+              .remove();
+
+          self.g.append("g")
+              .call(d3.axisLeft(self.y))
+            .append("text")
+              .attr("fill", "#000")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", "0.71em")
+              .attr("text-anchor", "end")
+              .text("Price ($)");
+
+          self.g.append("path")
+              .datum(data)
+              .attr("fill", "none")
+              .attr("stroke", "steelblue")
+              .attr("stroke-linejoin", "round")
+              .attr("stroke-linecap", "round")
+              .attr("stroke-width", 1.5)
+              .attr("d", line);
+        });
+
+    }
+
+} 
+
+var plot = new D3Plot()
+
+plot.draw()
+
+
+
