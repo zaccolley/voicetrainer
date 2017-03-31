6e97fc9287604bc28e7de691f7c9c5ff2ddd0488
Rachel Dowdall <rjdowdall@gmail.com>
2017-03-31 13:30:47 +0100

    Add moving line to d3 chart

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -17,13 +17,12 @@ function drawBuffer( width, height, context, data ) {
     }
 }
 
-
-
 var parseTime = d3.timeParse("%d-%b-%y");
 
 class D3Plot {
-    constructor(){
+    constructor(frequencies){
         console.log("hello")
+        var self = this;
         var svg = d3.select("svg"),
             margin = {top: 20, right: 20, bottom: 30, left: 50}
 
@@ -32,22 +31,67 @@ class D3Plot {
         
         this.g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        
-
-        this.x = d3.scaleTime()
+        this.x = d3.scaleLinear()
             .rangeRound([0, this.width]);
 
         this.y = d3.scaleLinear()
             .rangeRound([this.height, 0]);
 
+        this.frequencies = frequencies;
+
+        this.g.append("g")
+              .attr("transform", "translate(0," + this.height + ")")
+              .call(d3.axisBottom(this.x))
+            .select(".domain")
+              .remove();
+
+        this.g.append("g")
+              .call(d3.axisLeft(this.y))
+            .append("text")
+              .attr("fill", "#000")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", "0.71em")
+              .attr("text-anchor", "end")
+              .text("");
+
+        this.line = d3.line()
+            .x(function(d) { return self.x(d[0]); })
+            .y(function(d) { return self.y(d[1]); });
+
+
+        
+        /*this.line = d3.line()
+            .x(function(d) { return self.x(d[0]); })
+            .y(function(d) { return self.y(d[1]); });
+*/
+        //this.x.domain(d3.extent(frequencies, function(d) { return d[0]; }));
+        //this.y.domain(0, 200);
+
+
     }
 	
 
+    draw(data){
+        var self = this;
+        var zipdata = _.zip(self.frequencies, data)
+        
+        self.x.domain(d3.extent(zipdata, function(d) { return d[0]; }));
+        self.y.domain(d3.extent(zipdata, function(d) { return d[1]; }));
 
+        d3.selectAll("path").remove();
+        self.g.append("path")
+              .datum(zipdata)
+              .attr("fill", "none")
+              .attr("stroke", "steelblue")
+              .attr("stroke-linejoin", "round")
+              .attr("stroke-linecap", "round")
+              .attr("stroke-width", 1.5)
+              .attr("d", self.line);
 
+    }
 
-
-    draw(){
+    drawstock(){
 
         var self = this;
         var line = d3.line()
@@ -94,9 +138,8 @@ class D3Plot {
 
 } 
 
-var plot = new D3Plot()
-
-plot.draw()
+//var plot = new D3Plot()
+//plot.draw()
 
 
 
